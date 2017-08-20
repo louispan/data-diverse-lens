55986c9a412af89a7e90bbc38f2981b68c7be8cc
Louis Pan <louis@lory.local>
2017-08-20 10:39:38 +1000

    Updated to data-diverse v.0.10 with renamed reinterpretN'

--- src/Data/Diverse/Lens/Which.hs
+++ src/Data/Diverse/Lens/Which.hs
@@ -23,16 +23,10 @@ import Data.Diverse.TypeLevel
 
 -----------------------------------------------------------------
 
--- | Utility to convert Either to Maybe
-hush :: Either a b -> Maybe b
-hush = either (const Nothing) Just
-
------------------------------------------------------------------
-
 -- | 'pick' ('review' 'facet') and 'trial' ('preview' 'facet') in 'Prism'' form.
 --
 -- @
--- 'facet' = 'prism'' 'pick' (either (const Nothing) Just . 'trial')
+-- 'facet' = 'prism'' 'pick' ('trial'')
 -- @
 --
 -- @
@@ -41,9 +35,9 @@ hush = either (const Nothing) Just
 -- x \`shouldBe` (Just 5)
 -- @
 facet :: forall x xs. (UniqueMember x xs) => Prism' (Which xs) x
-facet = prism' pick (hush . trial)
+facet = prism' pick trial'
 
--- | 'pickL' ('review' 'facetL') and 'trialL' ('preview' 'facetL') in 'Prism'' form.
+-- | 'pickL' ('review' 'facetL') and 'trialL'' ('preview' 'facetL'') in 'Prism'' form.
 --
 -- @
 -- let y = 'review' ('facetL' \@Bar Proxy) (Tagged (5 :: Int)) :: Which '[Tagged Foo Bool, Tagged Bar Int, Char, Bool, Char]
@@ -51,12 +45,12 @@ facet = prism' pick (hush . trial)
 -- x \`shouldBe` (Just (Tagged 5))
 -- @
 facetL :: forall l xs x proxy. (UniqueLabelMember l xs, x ~ KindAtLabel l xs) => proxy l -> Prism' (Which xs) x
-facetL p = prism' (pickL p) (hush . trialL p)
+facetL p = prism' (pickL p) (trialL' p)
 
 -- | 'pickN' ('review' 'facetN') and 'trialN' ('preview' 'facetN') in 'Prism'' form.
 --
 -- @
--- 'facetN' p = 'prism'' ('pickN' p) (either (const Nothing) Just . 'trialN' p)
+-- 'facetN' p = 'prism'' ('pickN' p) ('trialN'' p)
 -- @
 --
 -- @
@@ -65,12 +59,12 @@ facetL p = prism' (pickL p) (hush . trialL p)
 -- x \`shouldBe` (Just 5)
 -- @
 facetN :: forall n xs x proxy. (MemberAt n x xs) => proxy n -> Prism' (Which xs) x
-facetN p = prism' (pickN p) (hush . trialN p)
+facetN p = prism' (pickN p) (trialN' p)
 
 ------------------------------------------------------------------
 
 
--- | 'diversify' ('review' 'inject') and 'reinterpret' ('preview' 'inject') in 'Prism'' form.
+-- | 'diversify' ('review' 'inject') and 'reinterpret'' ('preview' 'inject') in 'Prism'' form.
 --
 -- @
 -- let x = 'pick' (5 :: Int) :: 'Which' '[String, Int]
@@ -82,12 +76,12 @@ facetN p = prism' (pickN p) (hush . trialN p)
 inject
     :: forall branch tree.
        ( Diversify tree branch
-       , Reinterpret branch tree
+       , Reinterpret' branch tree
        )
     => Prism' (Which tree) (Which branch)
-inject = prism' diversify (hush . reinterpret)
+inject = prism' diversify reinterpret'
 
--- | 'diversifyL' ('review' 'injectL') and 'reinterpretL' ('preview' 'injectL') in 'Prism'' form.
+-- | 'diversifyL' ('review' 'injectL') and 'reinterpretL'' ('preview' 'injectL') in 'Prism'' form.
 --
 -- @
 -- let t = 'pick' \@[Tagged Bar Int, Tagged Foo Bool, Tagged Hi Char, Tagged Bye Bool] (5 :: Tagged Bar Int)
@@ -100,15 +94,15 @@ inject = prism' diversify (hush . reinterpret)
 injectL
     :: forall ls branch tree proxy.
        ( Diversify tree branch
-       , Reinterpret branch tree
+       , Reinterpret' branch tree
        , branch ~ KindsAtLabels ls tree
        , UniqueLabels ls tree
        , IsDistinct ls
        )
     => proxy ls -> Prism' (Which tree) (Which branch)
-injectL p = prism' (diversifyL p) (hush . reinterpretL p)
+injectL p = prism' (diversifyL p) (reinterpretL' p)
 
--- | 'diversifyN' ('review' 'injectN') and 'reinterpretN' ('preview' 'injectN') in 'Prism'' form.
+-- | 'diversifyN' ('review' 'injectN') and 'reinterpretN'' ('preview' 'injectN') in 'Prism'' form.
 --
 -- @
 -- let x = 'pick' (5 :: Int) :: 'Which' '[String, Int]
@@ -120,7 +114,7 @@ injectL p = prism' (diversifyL p) (hush . reinterpretL p)
 injectN
     :: forall indices branch tree proxy.
        ( DiversifyN indices tree branch
-       , ReinterpretN indices branch tree
+       , ReinterpretN' indices branch tree
        )
     => proxy indices -> Prism' (Which tree) (Which branch)
-injectN p = prism' (diversifyN p) (reinterpretN p)
+injectN p = prism' (diversifyN p) (reinterpretN' p)
